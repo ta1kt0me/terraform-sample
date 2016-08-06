be79e61e948b72ea6db744da1a608199ef2e109e
ta1kt0me <p.wadachi@gmail.com>
2016-08-07 01:04:21 +0900

    Enable dns hostname

--- sample.tf
+++ sample.tf
@@ -17,7 +17,7 @@ resource "aws_vpc" "sampleVPC" {
 		cidr_block = "10.1.0.0/16"
 		instance_tenancy = "default"
 		enable_dns_support = "true"
-		enable_dns_hostnames = "false"
+		enable_dns_hostnames = "true"
 		tags {
 				Name = "${var.tag}"
 		}
